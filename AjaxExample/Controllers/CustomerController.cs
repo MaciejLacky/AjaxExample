a049f6bfccfad8ae8b03da3fed8f186c76ba9d03
Maciej Łącki <maciej.lacky@gmail.com>
2021-05-22 20:11:06 +0200

    Add project files.

---
+++ AjaxExample/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using AjaxExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxExample.Controllers
+{
+    public class CustomerController : Controller
+    {
+        List<CustomerModel> customers = new List<CustomerModel>();
+
+        public CustomerController()
+        {
+            customers.Add(new CustomerModel { Id = 0, Name = "Maciej", Age = 35 });
+            customers.Add(new CustomerModel { Id = 1, Name = "Magda", Age = 30 });
+            customers.Add(new CustomerModel { Id = 2, Name = "Skiper", Age = 25 });
+            customers.Add(new CustomerModel { Id = 3, Name = "Tomasz", Age = 15 });
+            customers.Add(new CustomerModel { Id = 4, Name = "Kinga", Age = 32 });
+        }
+
+        public IActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public IActionResult ShowOnePerson(int Id)
+        {
+            CustomerModel c = customers.FirstOrDefault(x => x.Id == Id);
+            return PartialView(c);
+        }
+
+
+    }
+}
---
+++ AjaxExample/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(function () {
+
+    $(".customerRadio").change(function () {
+        doCustomerUpdate();
+    });
+
+    function doCustomerUpdate() {
+
+        $.ajax({
+            type: "POST",
+            url: "Customer/ShowOnePerson",
+            data: $("form").serialize(),
+            success: function (data) {
+             
+                $("#CustomerInformationArea").html(data);
+            }
+        });
+    };
+
+    $("#selectCustomer").click(function () {
+        event.preventDefault();
+        doCustomerUpdate();
+
+    });
+});
